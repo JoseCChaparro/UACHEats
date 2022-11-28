152cdbd280595cef35aa3341fa080ad5157a6c09
JOSE CARLOS CHAPARRO MORALES <a329613@uach.mx>
2022-11-28 16:44:07 -0600

    Initial commit

---
+++ config/googleData.js
@@ -0,0 +1,2 @@
+module.exports.clientId = "491677974840-56nh6jkbt60b7li89jusjr1of4nt18va.apps.googleusercontent.com";
+module.exports.clientSecret = "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";
---
+++ public/scripts/fooditem.js
@@ -0,0 +1,265 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Asado de puerco',
+    category : 'burrito',
+    price: 20,
+    img: 'images/menu/burritos/burrito_asado.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Chille beans',
+    category : 'burrito',
+    price: 20,
+    img: 'images/menu/burritos/burrito_chillibeans.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Chicharron',
+    category : 'burrito',
+    price: 20,
+    img: 'images/menu/burritos/burrito_chicharron.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Pollo en chipotle',
+    category : 'burrito',
+    rating : 4.3,
+    price: 20,
+    img: 'images/menu/burritos/burrito_chipotle.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Frijoles',
+    category : 'burrito',
+    rating : 4.3,
+    price: 20,
+    img: 'images/menu/burritos/burrito_frijoles.jpg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Mole',
+    category : 'burrito',
+    rating : 4.3,
+    price: 20,
+    img: 'images/menu/burritos/burrito_mole.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Picadillo',
+    category : 'burrito',
+    rating : 4.3,
+    price: 20,
+    img: 'images/menu/burritos/burrito_picadillo.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Asado de puerco',
+    category : 'montado',
+    rating : 4.3,
+    price: 30,
+    img: 'images/menu/montado.jpg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Chicharron',
+    category : 'montado',
+    rating : 4.3,
+    price: 30,
+    img: 'images/menu/montado.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Chile-beans',
+    category : 'montado',
+    rating : 4.3,
+    price: 30,
+    img: 'images/menu/montado.jpg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Pollo en chipotle',
+    category : 'montado',
+    rating : 4.3,
+    price: 30,
+    img: 'images/menu/montado.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Frijoles',
+    category : 'montado',
+    rating : 4.3,
+    price: 30,
+    img: 'images/menu/montado.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Mole',
+    category : 'montado',
+    rating : 4.3,
+    price: 30,
+    img: 'images/menu/montado.jpg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Picadillo',
+    category : 'montado',
+    rating : 4.3,
+    price: 30,
+    img: 'images/menu/montado.jpg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Sencillo',
+    category : 'mollete',
+    rating : 4.3,
+    price: 30,
+    img: 'images/menu/mollete_sencillo.jpg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Especial',
+    category : 'mollete',
+    rating : 4.3,
+    price: 40,
+    img: 'images/menu/mollete_especial.jpg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Hamburguesa',
+    category : 'hamburguesa',
+    rating : 4.3,
+    price: 60,
+    img: 'images/menu/hamburguesa.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Jamon',
+    category : 'torta',
+    rating : 4.3,
+    price: 25,
+    img: 'images/menu/tortas/torta_jamon.png',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Lomo',
+    category : 'torta',
+    rating : 4.3,
+    price: 50,
+    img: 'images/menu/tortas/torta_lomo.jpg',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Salchicha',
+    category : 'torta',
+    rating : 4.3,
+    price: 25,
+    img: 'images/menu/tortas/torta_salchicha.png',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Hot dog',
+    category : 'hotdog',
+    rating : 4.3,
+    price: 30,
+    img: 'images/menu/hotdog.png',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Platillo',
+    category : 'platillo',
+    rating : 4.3,
+    price: 40,
+    img: 'images/menu/platillo.jpg',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Sushi',
+    category : 'sushi',
+    rating : 4.3,
+    price: 50,
+    img: 'images/menu/sushi.jpg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Fruta y verdura',
+    category : 'frutaVerdura',
+    rating : 4.3,
+    price: 20,
+    img: 'images/menu/fruta_verdura.jpg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Sandwich',
+    category : 'sandwich',
+    rating : 4.3,
+    price: 20,
+    img: 'images/menu/sandwich.jpg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Baguete',
+    category : 'baguete',
+    rating : 4.3,
+    price: 25,
+    img: 'images/menu/baguete.jpg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Bola de arroz',
+    category : 'Bola_arroz',
+    rating : 4.3,
+    price: 60,
+    img: 'images/menu/bola_arroz.jpg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Sincronizada',
+    category : 'sincronizada',
+    rating : 4.3,
+    price: 20,
+    img: 'images/menu/sincronizada.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Chilaquiles',
+    category : 'chilaquiles',
+    rating : 4.3,
+    price: 30,
+    img: 'images/menu/chilaquiles.png',
+    quantity: 1
+},
+]
+
+export {foodItem};
